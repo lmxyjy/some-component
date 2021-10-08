65b0d41aecb6957e3105e44bd84d4faf8101296d
lmxyjy <yjy15680489038@163.com>
2021-10-08 11:49:38 +0800

    feat: add useBoolean

--- hooks/useBoolean.ts
+++ hooks/useBoolean.ts
@@ -1,1 +1,23 @@
 // 优雅的管理 boolean 值的 Hook。
+import { useMemo } from "react"
+import useToggle from "./useToggle"
+
+export interface Actions {
+  setTrue:() => void;
+  setFalse:() => void;
+  toggle:(value?:boolean | undefined) => void
+}
+
+const useBoolean = (defaltValue = false):[boolean,Actions] => {
+  const [state,{ toggle } ] = useToggle(defaltValue)
+
+  const actions = useMemo(() => ({
+    setTrue:() => toggle(true),
+    setFalse:() => toggle(false),
+    toggle
+  }),[])
+
+  return [state,actions]
+}
+
+export default useBoolean
