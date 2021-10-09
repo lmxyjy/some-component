c8575a99e404946789b92446f603308390198321
lmxyjy <yjy15680489038@163.com>
2021-10-09 13:38:58 +0800

    feat: add useUnmountedRef && fix dir name

---
+++ about-ahooks/useUnmountedRef.ts
@@ -0,0 +1,16 @@
+// 获取当前组件是否已经卸载的 hook，用于避免因组件卸载后更新状态而导致的内存泄漏
+import { useEffect,useRef } from "react"
+
+const useUnmountedRef = () => {
+  const unmountedRef = useRef(false)
+  useEffect(() => {
+    unmountedRef.current = false
+    return () => {
+      unmountedRef.current = true
+    }
+  },[])
+
+  return unmountedRef.current
+}
+
+export default useUnmountedRef
